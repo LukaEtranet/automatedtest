25915c3d163a8e939cd30cf87d6ea0fa630e2899
Tomislav <tstrelar@gmail.com>
2023-03-25 12:45:35 +0100

    Fix Chrome driver issue with allow origins

--- src/main/java/SelIntroduction.java
+++ src/main/java/SelIntroduction.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SelIntroduction {
     public static void main (String[] args) {
@@ -9,9 +10,15 @@ public class SelIntroduction {
         // WebDriver close get
         //WebDriver methods + class methhods
         //chromedriver.exe
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+
+        // !!! ne treba jer selenium iz mavena vec dolazi s chrome driverom !!!
+//        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+
         //webdriver.chrome.driver -> value of path
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://app.omnistore.kdev.billy.hr");
         driver.getTitle();
         System.out.println(driver.getTitle());
